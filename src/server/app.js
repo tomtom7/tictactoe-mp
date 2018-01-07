4c9ef0d99aae40722e71a89dbe86f98a6446f919
Toomas <>
2018-01-07 23:50:06 +0200

    split up server and app files

---
+++ src/server/app.js
@@ -0,0 +1,107 @@
+import uuid from 'node-uuid';
+import Player from './player';
+import Game from './game';
+
+export default class App {
+	constructor(io) {
+		this.io = io;
+		this.addSocketHandlers();
+		this.games = [];
+		this.players = [];
+	}
+
+	addSocketHandlers() {
+		this.io.sockets.on('connection', socket => {
+			socket.on('disconnect', () => this.onDisconnect(socket));
+			socket.on('playerJoin', () => this.onPlayerJoin(socket.id));
+			socket.on('playerInput', data => this.onPlayerInput(socket, data));
+			socket.on('playerLeave', () => this.onPlayerLeave(socket));
+		});
+	}
+
+	onDisconnect(socket) {
+		console.log(`Player left: ${socket.id}`);
+
+		this.games.forEach(game => {
+			game.players.forEach(player => {
+				if (player.id === socket.id) {
+					game.onOpponentLeft();
+					this.endGame(game);
+					console.log(`Game ${game.id} over, player: ${player.id} left`);
+				}
+			});
+		});
+		this.players.filter(p => p.id !== socket.id);
+	}
+
+	onPlayerJoin(id) {
+		console.log(`New player: ${id}`);
+		this.players.push(new Player(id));
+
+		if (this.players.length >= 2) {
+			this.createGame();
+		}
+	}
+
+	onPlayerLeave(socket) {
+		this.players = this.players.filter(p => p.id !== socket.id);
+	}
+
+	createGame() {
+		const player1 = this.players.shift();
+		const player2 = this.players.shift();
+
+		const game = new Game(uuid.v4(), player1, player2);
+
+		if (this.io.sockets.connected[player1.id] && this.io.sockets.connected[player2.id]) {
+			this.startGame(game);
+		}
+	}
+
+	findGame(id, playerId) {
+		return this.games.find(game => game.id === id && game.players.some(player => player.id === playerId));
+	}
+
+	onPlayerInput(socket, data) {
+		const game = this.findGame(data.gameId, socket.id);
+
+		if (!game) {
+			return;
+		}
+
+		game.validateTurn(socket.id, data.coordinates);
+
+		if (game.result) {
+			this.endGame(game);
+		} else {
+			this.updateGame(game);
+		}
+	}
+
+	addPlayerToGameRoom(playerId, gameId) {
+		this.io.sockets.connected[playerId].join(gameId);
+	}
+
+	removePlayersFromGameRoom(game) {
+		game.players.forEach(player => {
+			if (this.io.sockets.connected[player.id]) {
+				this.io.sockets.connected[player.id].leave(game.id);
+			}
+		});
+	}
+
+	endGame(game) {
+		this.io.to(game.id).emit('gameOver', game);
+		this.removePlayersFromGameRoom(game);
+	}
+
+	updateGame(game) {
+		this.io.to(game.id).emit('updateGame', game);
+	}
+
+	startGame(game) {
+		game.players.forEach(player => this.addPlayerToGameRoom(player.id, game.id));
+		this.games.push(game);
+		this.updateGame(game);
+	}
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,127 +1,19 @@
-import express from 'express';
-import socketIO from 'socket.io';
 import http from 'http';
 import path from 'path';
-import uuid from 'node-uuid';
-import Player from './player';
-import Game from './game';
+import express from 'express';
+import socketIO from 'socket.io';
+import App from './app';
 
+const port = process.env.PORT || 3000;
 const app = express();
-const httpServer = http.Server(app);
-const port = 3000;
+const server = http.Server(app);
+const io = socketIO(server);
+const DIST_DIR = path.resolve("dist");
 
-app.get('/', (req, res) => {
-	res.sendFile(path.resolve('dist/index.html'));
-});
+server.listen(port, () => console.log('listening on *:3000'));
 
-app.use('/dist', express.static(path.resolve('dist')));
+app.get('/', (req, res) => res.sendFile(path.join(DIST_DIR, "index.html")));
+app.use(express.static(DIST_DIR));
 
-httpServer.listen(port, () => {
-	console.log('listening on *:3000');
-});
+new App(io);
 
-class Server {
-	constructor() {
-		this.io = socketIO(httpServer);
-		this.addSocketHandlers();
-		this.games = [];
-		this.waitingPlayers = [];
-	}
-
-	addSocketHandlers() {
-		this.io.sockets.on('connection', socket => {
-			socket.on('disconnect', () => this.onDisconnect(socket));
-			socket.on('playerJoin', () => this.onPlayerJoin(socket.id));
-			socket.on('playerInput', data => this.onPlayerInput(socket, data));
-			socket.on('playerLeave', () => this.onPlayerLeave(socket));
-		});
-	}
-
-	onDisconnect(socket) {
-		console.log(`Player left: ${socket.id}`);
-
-		this.games.forEach(game => {
-			game.players.forEach(player => {
-				if (player.id === socket.id) {
-					game.onOpponentLeft();
-					this.endGame(game);
-					console.log(`Game ${game.id} over, player: ${player.id} left`);
-				}
-			});
-		});
-		this.waitingPlayers.filter(p => p.id !== socket.id);
-	}
-
-	onPlayerJoin(id) {
-		console.log(`New player: ${id}`);
-		this.waitingPlayers.push(new Player(id));
-
-		if (this.waitingPlayers.length >= 2) {
-			this.createGame();
-		}
-	}
-
-	onPlayerLeave(socket) {
-		this.waitingPlayers = this.waitingPlayers.filter(p => p.id !== socket.id);
-	}
-
-	createGame() {
-		const player1 = this.waitingPlayers.shift();
-		const player2 = this.waitingPlayers.shift();
-
-		const game = new Game(uuid.v4(), player1, player2);
-
-		if (this.io.sockets.connected[player1.id] && this.io.sockets.connected[player2.id]) {
-			this.startGame(game);
-		}
-	}
-
-	findGame(id, playerId) {
-		return this.games.find(game => game.id === id && game.players.some(player => player.id === playerId));
-	}
-
-	onPlayerInput(socket, data) {
-		const game = this.findGame(data.gameId, socket.id);
-
-		if (!game) {
-			return;
-		}
-
-		game.validateTurn(socket.id, data.coordinates);
-
-		if (game.result) {
-			this.endGame(game);
-		} else {
-			this.updateGame(game);
-		}
-	}
-
-	addPlayerToGameRoom(playerId, gameId) {
-		this.io.sockets.connected[playerId].join(gameId);
-	}
-
-	removePlayersFromGameRoom(game) {
-		game.players.forEach(player => {
-			if (this.io.sockets.connected[player.id]) {
-				this.io.sockets.connected[player.id].leave(game.id);
-			}
-		});
-	}
-
-	endGame(game) {
-		this.io.to(game.id).emit('gameOver', game);
-		this.removePlayersFromGameRoom(game);
-	}
-
-	updateGame(game) {
-		this.io.to(game.id).emit('updateGame', game);
-	}
-
-	startGame(game) {
-		game.players.forEach(player => this.addPlayerToGameRoom(player.id, game.id));
-		this.games.push(game);
-		this.updateGame(game);
-	}
-}
-
-new Server();
